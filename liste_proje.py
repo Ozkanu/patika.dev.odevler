3b36eb796324f051d90a26dd50ba52df9e253c82
Ozkanu <60445475+Ozkanu@users.noreply.github.com>
2022-02-12 21:31:31 +0300

    Update and rename duz_liste.py to liste_proje.py

--- liste_proje.py
+++ liste_proje.py
@@ -1,3 +1,4 @@
+#birinci kısım 
 duz=[]
 
 def duz_liste(liste):
@@ -11,3 +12,10 @@ liste_1= [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 duz_liste(liste_1)
 
 print(duz)
+
+# ikinci kısım
+y=[[1, 2], [3, 4], [5, 6, 7]]
+new=list()
+for i in sorted(y,reverse=True):
+    new.append(sorted(i, reverse=True))
+print(new)
